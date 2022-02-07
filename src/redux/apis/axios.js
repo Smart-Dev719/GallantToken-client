41c03001e2881d7f384aa9a9fd6222cfd01880af
Smart-Dev719 <blockchaindev719@gmail.com>
2022-02-07 04:34:51 -0500

    feat: final update

--- src/redux/apis/axios.js
+++ src/redux/apis/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const authAxios = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL_LOCAL, //YOUR_API_URL HERE
+  baseURL: process.env.REACT_APP_SERVER_URL, //YOUR_API_URL HERE
   mode: 'no-cors',
   headers: {
     'Content-Type': 'application/json',
